50a8744a84864cad5d3c3864dc7f2f7a0828bd17
luis ALBUQUERQUE <luis.albuquerque@aled.local>
2026-02-03 00:43:23 -0300

    atualização de dependencias

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -20,7 +20,9 @@ if config_env() == :prod do
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
-    ]
+    ],
+    url: [host: "syslumen.aled1.com", port: 443, scheme: "https"],
+    check_origin: false
 end
 
 # Google Sheets Configuration
